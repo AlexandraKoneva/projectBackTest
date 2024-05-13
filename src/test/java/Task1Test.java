3469e082bbce3b306d00a8b045a5d2dbe603ba60
aleksandrakoneva <akoneva@alfabank.ru>
2024-05-13 08:44:37 +0300

    3.5 commit

--- src/test/java/Task1Test.java
+++ src/test/java/Task1Test.java
@@ -18,13 +18,13 @@ public class Task1Test {
     @Test
     public void readFirstTest() {
         step("Получение одного post", () -> {
-        PostsDataResponse postsData = given()
-                .when()
-                .contentType(ContentType.JSON)
-                .get(URL + "posts/1")
-                .then().log().all()
-                .statusCode(200)
-                .extract().as(PostsDataResponse.class);
+            PostsDataResponse postsData = given()
+                    .when()
+                    .contentType(ContentType.JSON)
+                    .get(URL + "posts/1")
+                    .then().log().all()
+                    .statusCode(200)
+                    .extract().as(PostsDataResponse.class);
 
 
             Assertions.assertEquals(1, postsData.getId(), "Был взят не первый элемент");
@@ -35,99 +35,99 @@ public class Task1Test {
     @Test
     public void readSecondTest() {
         step("Получение всех posts", () -> {
-        List<PostsDataResponse> postsData = given()
-                .when()
-                .contentType(ContentType.JSON)
-                .get(URL + "posts")
-                .then().log().all()
-                .statusCode(200)
-                .extract().jsonPath().getList("", PostsDataResponse.class);
+            List<PostsDataResponse> postsData = given()
+                    .when()
+                    .contentType(ContentType.JSON)
+                    .get(URL + "posts")
+                    .then().log().all()
+                    .statusCode(200)
+                    .extract().jsonPath().getList("", PostsDataResponse.class);
 
-        Assertions.assertEquals(100, postsData.size(), "Пришли не все элементы");
+            Assertions.assertEquals(100, postsData.size(), "Пришли не все элементы");
         });
     }
 
     @Test
     public void createPostTest() {
         step("Создание post", () -> {
-        PostsDataRequest postDataRequest = PostsDataRequest.builder()
-                                                           .title("foo")
-                                                           .body("bar")
-                                                           .userId(1)
-                                                           .build();
+            PostsDataRequest postDataRequest = PostsDataRequest.builder()
+                                                               .title("foo")
+                                                               .body("bar")
+                                                               .userId(1)
+                                                               .build();
 
-        PostsDataResponse createdPost = given()
-                .contentType(ContentType.JSON)
-                .body(postDataRequest)
-                .when()
-                .post(URL + "posts")
-                .then().log().all()
-                .statusCode(201)
-                .extract().as(PostsDataResponse.class);
+            PostsDataResponse createdPost = given()
+                    .contentType(ContentType.JSON)
+                    .body(postDataRequest)
+                    .when()
+                    .post(URL + "posts")
+                    .then().log().all()
+                    .statusCode(201)
+                    .extract().as(PostsDataResponse.class);
 
-        Assertions.assertEquals(1, createdPost.getUserId(), "UserId не совпадает");
-        Assertions.assertEquals(101, createdPost.getId(), "Id не совпадает");
-        Assertions.assertEquals("foo", createdPost.getTitle(), "Title не совпадает");
-        Assertions.assertEquals("bar", createdPost.getBody(), "Body не совпадает");
+            Assertions.assertEquals(1, createdPost.getUserId(), "UserId не совпадает");
+            Assertions.assertEquals(101, createdPost.getId(), "Id не совпадает");
+            Assertions.assertEquals("foo", createdPost.getTitle(), "Title не совпадает");
+            Assertions.assertEquals("bar", createdPost.getBody(), "Body не совпадает");
         });
     }
 
     @Test
     public void putPostTest() {
         step("Обновление всего post", () -> {
-        PostsDataRequest postDataRequest = PostsDataRequest.builder()
-                                                           .title("foo")
-                                                           .body("bar")
-                                                           .userId(4)
-                                                           .build();
+            PostsDataRequest postDataRequest = PostsDataRequest.builder()
+                                                               .title("foo")
+                                                               .body("bar")
+                                                               .userId(4)
+                                                               .build();
 
-        PostsDataResponse updatedPost = given()
-                .contentType(ContentType.JSON)
-                .body(postDataRequest)
-                .when()
-                .put(URL + "posts/1")
-                .then().log().all()
-                .statusCode(200)
-                .extract().as(PostsDataResponse.class);
+            PostsDataResponse updatedPost = given()
+                    .contentType(ContentType.JSON)
+                    .body(postDataRequest)
+                    .when()
+                    .put(URL + "posts/1")
+                    .then().log().all()
+                    .statusCode(200)
+                    .extract().as(PostsDataResponse.class);
 
-        Assertions.assertEquals(1, updatedPost.getId(), "Id не совпадает");
-        Assertions.assertEquals("foo", updatedPost.getTitle(), "Title не совпадает");
-        Assertions.assertEquals("bar", updatedPost.getBody(), "Body не совпадает");
-        Assertions.assertEquals(4, updatedPost.getUserId(), "UserId не совпадает");
+            Assertions.assertEquals(1, updatedPost.getId(), "Id не совпадает");
+            Assertions.assertEquals("foo", updatedPost.getTitle(), "Title не совпадает");
+            Assertions.assertEquals("bar", updatedPost.getBody(), "Body не совпадает");
+            Assertions.assertEquals(4, updatedPost.getUserId(), "UserId не совпадает");
         });
     }
 
     @Test
     public void patchPostTest() {
         step("Обновление части post", () -> {
-        PostsDataRequest postDataRequest = PostsDataRequest.builder()
-                                                           .title("foo")
-                                                           .userId(16)
-                                                           .build();
+            PostsDataRequest postDataRequest = PostsDataRequest.builder()
+                                                               .title("foo")
+                                                               .userId(16)
+                                                               .build();
 
-        PostsDataResponse updatedPost = given()
-                .contentType(ContentType.JSON)
-                .body(postDataRequest)
-                .when()
-                .patch(URL + "posts/1")
-                .then().log().all()
-                .statusCode(200)
-                .extract().as(PostsDataResponse.class);
+            PostsDataResponse updatedPost = given()
+                    .contentType(ContentType.JSON)
+                    .body(postDataRequest)
+                    .when()
+                    .patch(URL + "posts/1")
+                    .then().log().all()
+                    .statusCode(200)
+                    .extract().as(PostsDataResponse.class);
 
-        Assertions.assertEquals(1, updatedPost.getId(), "Id не совпадает");
-        Assertions.assertEquals("foo", updatedPost.getTitle(), "Title не совпадает");
-        Assertions.assertEquals(16, updatedPost.getUserId(), "UserId не совпадает");
+            Assertions.assertEquals(1, updatedPost.getId(), "Id не совпадает");
+            Assertions.assertEquals("foo", updatedPost.getTitle(), "Title не совпадает");
+            Assertions.assertEquals(16, updatedPost.getUserId(), "UserId не совпадает");
         });
     }
 
     @Test
     public void deletePostTest() {
         step("Удаление post", () -> {
-        given()
-                .when()
-                .delete(URL + "posts/1")
-                .then().log().all()
-                .statusCode(200);
+            given()
+                    .when()
+                    .delete(URL + "posts/1")
+                    .then().log().all()
+                    .statusCode(200);
         });
     }
 }
